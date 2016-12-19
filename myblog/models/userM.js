87b275932db86711ad39a0bf139bfd0de08462f9
18645101605 <1223923223@qq.com>
2016-12-19 16:00:31 +0800

    myblog（练习版）

---
+++ myblog/models/userM.js
@@ -0,0 +1,10 @@
+var db=require('./db');
+
+exports.queryByNameAndPwd=function(username,password,callback){
+    var sql="select * from t_user where username=? and password=?";
+    db.query(sql,[username,password],callback);
+};
+exports.save=function(username,password,email,callback){
+    var sql='insert into t_user(username,password,email) values(?,?,?)';
+    db.query(sql,[username,password,email],callback);
+};
---
+++ myblog/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var user=require('../controllers/user');
+
+/* GET home page. */
+router.get('/',user.index);
+router.get('/login',user.login);
+router.get('/reg',user.reg);
+router.post('/regist',user.regist);
+router.post('/checkLogin',user.checkLogin);
+
+module.exports = router;
